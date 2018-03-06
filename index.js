a2fbcc33497b73fdeb0308c4780e38171bfd9320
William Park <wjypark94@gmail.com>
2018-03-05 17:44:48 -0800

    thinkful tube a11y refactor

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var $ = require('jquery');
+let $ = require('jquery');
 
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-const apiKey = 'AIzaSyB6LDXCfWtsPmqUn9qLGPtPvoluS0tKmBE';
 
-function getDataFromApi(searchTerm, callback) {
+const apiKey = 'AIzaSyCmXbr84NI-eo7eCHQTcf7iJc1PSGsjsuc';
+
+function getDataFromApi(searchTerm, callback){
   const settings = {
     url: YOUTUBE_SEARCH_URL,
     data: {
@@ -15,38 +16,44 @@ function getDataFromApi(searchTerm, callback) {
     type: 'GET',
     success: callback
   };
-$.ajax(settings);
+  $.ajax(settings);
 }
 
-function displayResult(result) {
+function displayResult(result){
   console.log('displayResult', result);
   return `
-  <div>
-	  <h2>
-		<a class="js-result-name" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a> 
-		</h2>
-	  <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.medium.url}"></a>
-	</div> 
-      `;
+    <ul>
+      <li>    
+        <h3 class="js-video-title">${result.snippet.title}</h3>
+        <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img class="js-result-img" src="${result.snippet.thumbnails.medium.url}" alt="${result.snippet.title}"></a>
+        <a href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank">More from this channel: ${result.snippet.channelTitle}</a>
+      </li>
+    </ul>  
+  `;
 }
 
-function displayYoutubeSearchResults(data) {
- const searchResults = data.items.map((item, index) => displayResult(item));
- console.log('displayYoutubeSearchResults', searchResults);
- $('.js-search-results').html(searchResults);
+function displayYoutubeSearchResults(data){
+  const searchResults = data.items.map((item, index) => displayResult(item));
+  console.log('displayYoutubeSearchResults', searchResults);
+  $('.js-search-results').html(searchResults);
 }
 
 function watchSubmit() {
-  $('.js-search-form').submit(function(event) {
+  $('.js-search-form').submit(function(event){
     event.preventDefault();
-    let queryTarget = $(event.currentTarget).find('js-query');
+    let queryTarget = $(event.currentTarget).find('.js-query');
     let query = queryTarget.val();
     console.log('query', query);
     queryTarget.val("");
     getDataFromApi(query, displayYoutubeSearchResults);
   });
 }
+
+
+
 $(watchSubmit);
+
+
 
 
 
